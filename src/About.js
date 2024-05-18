fccc671f8e0308cc94dda84996c92a0590253619
Angela Bello <angela.52911@gmail.com>
2024-05-18 05:35:48 -0500

    Gallery

--- src/About.js
+++ src/About.js
@@ -65,7 +65,7 @@ function About() {
                 </div>
 
                 <div className="framer">
-                <h1>Shop</h1>
+                <h1>Gallery</h1>
                     <p>Alonside design and web development, I love painting in oil on canvas. For several years, I dedicate to learn art techinicques and styles. My approach is realistic also incorporate illustraiton, and color alteration.</p>
                    
                     <a href="https://www.etsy.com/shop/AngelaBelloR" target="_blank" rel="noreferrer"><button>
