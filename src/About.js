1d756937ec82bdd75ea2e81b7bf88f61f8b35074
Angela Bello <angela.52911@gmail.com>
2024-12-21 18:16:04 -0600

    about spellling

--- src/About.js
+++ src/About.js
@@ -30,7 +30,7 @@ function About() {
                 <div className="framer">
 
                     <h1>About</h1>
-                    <p>I am a Visual Designer with a track record in creative solutions to challenging projects. What makes me unique is a background in engineering. As an engineer I developed critical thinking, problem-solving, time management, organizational, and interpersonal communication skills. This combination of education and experience enables me to approach visual design challenges with strategy.
+                    <p>I am a Visual Designer with a track record in creative solutions to challenging projects. What makes me unique is my background in engineering. As an engineer, I developed critical thinking, problem-solving, time management, organizational, and interpersonal communication skills. This combination of education and experience enables me to approach visual design challenges with strategy.
                     </p>
                 </div>
             </div>
@@ -42,9 +42,7 @@ function About() {
                     <h1>Experience</h1>
 
                     <p>
-                    My current role is Designer for WellSpa360 at Allured Media Bussines, I crafted captivating layouts for print and digital magazines, enabling readers to immerse themselves in content-rich articles. Even more, I contribute to elevating digital engagement through dynamic updates for both mobile and desktop views of the digital magazine. I also crafted interactive content, motion graphics, and animations. And  I aligned with marketing, innovating motion graphics, social media posts, animations, and web banners for high-impact events, webcasts, emails, and campaigns.
-                    Previously, as a freelancer, I created impactful digital content, meticulously embodying brand identity and editing images in Photoshop, amplifying their appeal to align with marketing strategies and user feedback.
-                    Another remarkable experience I had as a UI/UX Designer. I contributed to relaunching the intranet UI design by translating user needs into compelling and actionable recommendations derived from rigorous UX research synchronized with developers.
+                    My current role is Designer for WellSpa360 at Allured Media Bussines. I crafted captivating layouts for print and digital magazines. Even more, I contribute to elevating digital engagement through dynamic updates for digital magazine mobile and desktop views. I also crafted interactive content, motion graphics, and animations. I aligned with marketing, innovating motion graphics, social media posts, animations, and web banners for high-impact events, webcasts, emails, and campaigns. Previously, as a freelancer, I created impactful digital content, meticulously embodying brand identity and editing images in Photoshop, amplifying their appeal to align with marketing strategies and user feedback. Another remarkable experience I had as a UI/UX Designer. I contributed to relaunching the intranet UI design by translating user needs into compelling and actionable recommendations from rigorous UX research synchronized with developers.
                     </p>
                 </div>
                 <div className="framer">
@@ -66,7 +64,7 @@ function About() {
 
                 <div className="framer">
                 <h1>Gallery</h1>
-                    <p>Alongside design and web development, I love painting in oil on canvas. For several years, I dedicate to learn art techinicques and styles. My approach is realistic also incorporate illustration, and color alteration.</p>
+                    <p>Alongside design and web development, I love painting in oil on canvas. For several years,  I've learned art techniques and styles. My approach is realistic, illustrative, and colorfully displayed.</p>
                    
                    
              
